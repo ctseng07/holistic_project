faf9f8538fced9f9ba2d9c4e48d6fcede062f1c3
Caleb Tseng <calebtseng07@gmail.com>
2023-12-14 03:44:46 -0800

    Finish Blog section

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,12 +1,22 @@
+import { rightArrowBlack } from "../assets/icons"
+
 const BlogCard = ({ imgURL, title, content, action }) => {
     return (
         <div className="flex flex-1 flex-col w-full max-sm:w-full capitalize">
             <img src={imgURL} alt={title}
-                className="w-[280px] h-[280px]"
-            />
+                className="w-full h-[280px]"
+                />
             <h3 className='mt-2 text-xl leading-normal font-thin
             '>{title}</h3>
-            <button className='mt-2 font-thin capitalize text-sm leading-normal'>{action}</button>
+
+            <p className="mt-2 font-thin capitalize text-sm leading-normal">{content}</p>
+
+            <button className='mt-2 font-thin capitalize text-sm leading-normal flex items-end gap-1'>{action} 
+            <img src={rightArrowBlack} 
+            alt="rightArrow" 
+            width={20}
+            height={20}/></button>
+
         </div>
     )
 }
--- src/sections/Blog.jsx
+++ src/sections/Blog.jsx
@@ -1,26 +1,33 @@
 import { blogs } from "../constants";
 import BlogCard from "../components/BlogCard";
+import { shiftLeft, shiftRight } from "../assets/icons";
 
 const Blog = () => {
   return (
-<section id='blogs'
-className="max-container
+    <section id='blogs'
+      className="max-container
 max-sm:mt-12">
-  <div className="flex flex-col justify-start gap-5">
-<h2 className="text-4xl font-palanquin font-bold">Our 
-<span className="text-coral-red"> Popular</span> Products</h2>
-<p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
-  Experience top-notch quality and style with our sought-after
-   selections. Discover a world of comfort, design, and value</p>
-  </div>
+      <div className="flex justify-between">
+        <h6 className="font-proxima font-thin uppercase text-sm">blog</h6>
+        <button className="flex gap-3">
+          <img src={shiftLeft}
+            alt="shift-left"
+            height={20}
+            width={20} />
+          <img src={shiftRight}
+            alt="shift-right"
+            height={20}
+            width={20} />
+        </button>
+      </div>
 
-  <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 
+      <div className="mt-5 grid md:grid-cols-3 
   sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
-    {blogs.map((blog) => (
-      <BlogCard key={blog.title} {...blog} />
-    ))}
-  </div>
-</section>
+        {blogs.map((blog) => (
+          <BlogCard key={blog.title} {...blog} />
+        ))}
+      </div>
+    </section>
   )
 }
 
