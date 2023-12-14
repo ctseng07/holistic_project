635ef7ac489f70f4ab28881f280a40dfee25d72d
Caleb Tseng <calebtseng07@gmail.com>
2023-12-14 03:45:46 -0800

    Fix shop bug

--- src/sections/Shop.jsx
+++ src/sections/Shop.jsx
@@ -4,10 +4,10 @@ import Button from '../components/Button';
 
 const Shop = () => {
   return (
-    <div className='max-container flex justify-between items-center'>
+    <div className='max-container flex justify-between items-center max-lg:flex-col max-lg:gap-3'>
       <img src={ShopPic} alt="Shop" width={600}/>
 
-      <div className="flex-col flex items-center justify-center text-center w-1/2 gap-10">
+      <div className="flex-col flex items-center justify-center text-center w-1/2 gap-5">
         <p className='uppercase text-xs font-thin'>Shop</p>
 
         <h2 className='whitespace-pre-wrap text-4xl'>Restore Your Skin's Natural Balance</h2>
