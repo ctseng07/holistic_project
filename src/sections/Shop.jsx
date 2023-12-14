39350c8c12dd107702b8488bd9989a999d655075
Caleb Tseng <calebtseng07@gmail.com>
2023-12-14 05:13:37 -0800

    Fix media screen sizing

--- src/sections/Shop.jsx
+++ src/sections/Shop.jsx
@@ -4,13 +4,13 @@ import Button from '../components/Button';
 
 const Shop = () => {
   return (
-    <div className='max-container flex justify-between items-center max-lg:flex-col max-lg:gap-3'>
+    <div className='max-container flex justify-between items-center max-lg:flex-col max-lg:gap-3 max-lg:py-10 max-lg:px-6'>
       <img src={ShopPic} alt="Shop" width={600}/>
 
       <div className="flex-col flex items-center justify-center text-center w-1/2 gap-5">
         <p className='uppercase text-xs font-thin'>Shop</p>
 
-        <h2 className='whitespace-pre-wrap text-4xl'>Restore Your Skin's Natural Balance</h2>
+        <h2 className='whitespace-pre-wrap text-4xl max-lg:text-2xl'>Restore Your Skin's Natural Balance</h2>
 
         <Button label="Visit the mindful shop" />
       </div>
