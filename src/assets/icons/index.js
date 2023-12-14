83324cdbe0bb358fa154208dea08d6131d02ae79
Caleb Tseng <calebtseng07@gmail.com>
2023-12-14 03:45:20 -0800

    Add more files imgs and icons to folder

--- src/assets/icons/index.js
+++ src/assets/icons/index.js
@@ -1,6 +1,9 @@
 import hamburger from './hamburger.svg';
 import rightArrow from './right-arrow.svg';
+import rightArrowBlack from './right-arrow-black.svg';
 import leftArrow from './left-arrow.svg';
+import shiftRight from './shift-right.svg';
+import shiftLeft from './shift-left.svg';
 import arrowUp from './arrow-up.svg';
 import arrowDown from './arrow-down.svg';
 import user from './user.svg';
@@ -11,7 +14,10 @@ import instagram from './insta.svg';
 export {
     hamburger,
     rightArrow,
+    rightArrowBlack,
     leftArrow,
+    shiftRight,
+    shiftLeft,
     arrowUp,
     arrowDown,
     user,
